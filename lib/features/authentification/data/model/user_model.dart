b91a5944df305fb4db3e06f99043118908dac4cf
Nicolas_dmb <gyfeix@gmail.com>
2025-02-28 01:07:35 +0100

    modif de user model

--- lib/features/authentification/data/model/user_model.dart
+++ lib/features/authentification/data/model/user_model.dart
@@ -1,14 +1,32 @@
 import 'package:equatable/equatable.dart';
+import 'package:mywallet_mobile/features/authentification/domain/entities/user_signup.dart';
 
 class UserModel extends Equatable {
-  const UserModel({required this.username});
-
+  final String? email;
   final String username;
+  final String? password;
+  final String? confirmPassword;
+
+  const UserModel({
+    this.email,
+    required this.username,
+    this.password,
+    this.confirmPassword,
+  });
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
     return UserModel(username: json['username']);
   }
 
+  static Map<String, dynamic> toJson(UserSignup user) {
+    return {
+      'email': user.email,
+      'username': user.username,
+      'password': user.password,
+      'confirmPassword': user.confirmPassword,
+    };
+  }
+
   @override
-  List<Object?> get props => [username];
+  List<Object?> get props => [username, email, password, confirmPassword];
 }
